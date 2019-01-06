c059b5dcaf26de9c7e09529548aa56bf9a1f8f97
Jordan Hansen <jbhansen84@gmail.com>
2019-01-05 17:15:31 -0700

    hopefully don't duplicate

--- src/detailsPage.ts
+++ src/detailsPage.ts
@@ -5,7 +5,6 @@ import { getPropertyBySelector, setUpNewPage } from 'puppeteer-helpers';
 export async function scrapeDetailsPage(product: ElementHandle, browser: Browser, minimumAllowedNumberOfVendors: number, wantSoldByAmazon: boolean) {
     const page = await setUpNewPage(browser);
     const url = await getPropertyBySelector(product, '.s-access-detail-page', 'href');
-    const currentDate = new Date();
     await page.goto(url);
     let buyboxVendor = (await getPropertyBySelector(page, '#merchant-info a', 'innerHTML'));
     if (buyboxVendor) {
@@ -25,7 +24,7 @@ export async function scrapeDetailsPage(product: ElementHandle, browser: Browser
 
     // If it's sold by Amazon the data parsed from buyboxVendor will be 'Details', 'easy-to-open packaging', or null
     if ((!wantSoldByAmazon && buyboxVendor && (buyboxVendor !== 'Details' || buyboxVendor !== 'easy-to-open packaging')) && parseInt(numberOfVendors) >= minimumAllowedNumberOfVendors) {
-        const dataToReturn = { numberOfVendors: numberOfVendors, buyboxVendor: buyboxVendor, brand: brand, url: url, createdAt: currentDate, updatedAt: currentDate };
+        const dataToReturn = { numberOfVendors: numberOfVendors, buyboxVendor: buyboxVendor, brand: brand, url: url };
         await page.close();
 
         return Promise.resolve(dataToReturn);
