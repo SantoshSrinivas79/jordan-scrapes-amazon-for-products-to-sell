57c18f7381fdc8acac13584bc2f1c293026acce9
Jordan Hansen <jbhansen84@gmail.com>
2019-01-12 05:56:17 -0700

    Update webhook discord version, added a sample-config

---
+++ src/sample-config.ts
@@ -0,0 +1,8 @@
+export const config = {
+    mongoUser: "<mongo-user-name>",
+    mongoPass: "<mongo-password>",
+    mongoUrl: "ip.address.goes.here:portHere",
+    mongoDb: "<mongo-db-name>",
+    mongoCollection: "<mong-collection>",
+    webhookUrl: '<discord-webhook-url>'
+};
